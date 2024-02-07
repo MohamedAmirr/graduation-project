7130431a124a3eebca4daec745f096481eabcd4d
Mohamed Amir <m.elamir027@gmail.com>
2024-02-07 14:43:15 +0200

    generate list of random images

--- GP/send_command/views.py
+++ GP/send_command/views.py
@@ -1,38 +1,20 @@
 import base64
-import json
 from io import BytesIO
-
-from PIL import ImageDraw,Image
+from PIL import ImageDraw
 from django.shortcuts import render
-from diffusers import DiffusionPipeline, AutoencoderKL
-import torch
 from PIL import Image
-import matplotlib.pyplot as plt
 from django.views.decorators.csrf import csrf_exempt
-
 from .forms import MyForm
 
 
-# Create your views here.
-
-
-def generate_story(request):
-    pass
-
-
-def image_grid(imgs, rows, cols, resize=256):
-    assert len(imgs) == rows * cols
-
-    if resize is not None:
-        imgs = [img.resize((resize, resize)) for img in imgs]
-
-    plt.figure(figsize=(20, 20))
-
-    for index, image in enumerate(imgs):
-        ax = plt.subplot(1, len(imgs), index + 1)
-        plt.imshow(image)
-        plt.axis("off")
-    plt.show()
+def create_text_image():
+    image = Image.new('RGB', (200, 100), color='blue')
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), "Hello", fill="white")
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    return img_str
 
 
 @csrf_exempt
@@ -40,39 +22,12 @@ def generate_images(request):
     if request.method == "POST":
         form = MyForm(request.POST)
         if form.is_valid():
-            vae = AutoencoderKL.from_pretrained(
-                "madebyollin/sdxl-vae-fp16-fix",
-                # torch_dtype=torch.float32
-            )
+            image_list = []
+            image_list.append(create_text_image())
+            image_list.append(create_text_image())
 
-            pipe = DiffusionPipeline.from_pretrained(
-                "stabilityai/stable-diffusion-xl-base-1.0",
-                # vae=vae,
-                # torch_dtype=torch.float16,
-                # variant="fp16",
-                # use_safetensors=True,
-            )
-            # pipe = pipe.to("cuda")
-            prompt = form.cleaned_data['story']
-
-            images = pipe(prompt=prompt, num_inference_steps=1, num_images_per_prompt=1)
-            image = images.images[0]
-            # draw = ImageDraw.Draw(image)
-            # draw.text((10, 10), "Hello", fill="white")
-            # print(image.images)
-            # print(type(image.images))
-            buffered = BytesIO()
-            image.save(buffered, format="PNG")
-            img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-            # image_grid(image.images, 1, 1)
-            return render(request, 'index.html', {'generated_images': img_str})
+            return render(request, 'index.html', {'generated_images': image_list})
     else:
         form = MyForm()
 
     return render(request, 'index.html', {'form': form})
-
-
-def home_page(request):
-    if request.method == 'GET':
-        return render(request, 'index.html')
--- main.py
+++ main.py
@@ -1,51 +1,55 @@
-from diffusers import DiffusionPipeline, AutoencoderKL
-import torch
-from PIL import Image
-import matplotlib.pyplot as plt
+# from diffusers import DiffusionPipeline, AutoencoderKL
+# import torch
+# from PIL import Image
+# import matplotlib.pyplot as plt
+#
+# vae = AutoencoderKL.from_pretrained(
+#     "madebyollin/sdxl-vae-fp16-fix",
+#     # torch_dtype=torch.float32
+# )
+#
+# pipe = DiffusionPipeline.from_pretrained(
+#     "stabilityai/stable-diffusion-xl-base-1.0",
+#     # vae=vae,
+#     # torch_dtype=torch.float16,
+#     # variant="fp16",
+#     # use_safetensors=True,
+# )
+# # pipe = pipe.to("cuda")
+#
+# prompt = "A man in a spacesuit is running a marathon in the jungle."
+#
+# image = pipe(prompt=prompt, num_inference_steps=25, num_images_per_prompt=1)
+#
+#
+# def image_grid(imgs, rows, cols, resize=256):
+#     assert len(imgs) == rows * cols
+#
+#     if resize is not None:
+#         imgs = [img.resize((resize, resize)) for img in imgs]
+#
+#     plt.figure(figsize=(20, 20))
+#
+#     for index, image in enumerate(imgs):
+#         ax = plt.subplot(1, len(imgs), index + 1)
+#         plt.imshow(image)
+#         plt.axis("off")
+#     plt.show()
+#
+#
+# image_grid(image.images, 1, 1)
+from openai import OpenAI
 
-vae = AutoencoderKL.from_pretrained(
-    "madebyollin/sdxl-vae-fp16-fix",
-    # torch_dtype=torch.float32
+client = OpenAI(
+    api_key='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'
 )
-
-pipe = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0",
-    # vae=vae,
-    # torch_dtype=torch.float16,
-    # variant="fp16",
-    # use_safetensors=True,
+chat_completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system",
+         "content": "create prompts for a story to generate images and pictures"},
+        {"role": "user",
+         "content": "make a short story using Disney characters"}
+    ],
 )
-# pipe = pipe.to("cuda")
-
-prompt = "A man in a spacesuit is running a marathon in the jungle."
-
-image = pipe(prompt=prompt, num_inference_steps=25, num_images_per_prompt=1)
-
-
-def image_grid(imgs, rows, cols, resize=256):
-    assert len(imgs) == rows * cols
-
-    if resize is not None:
-        imgs = [img.resize((resize, resize)) for img in imgs]
-
-    plt.figure(figsize=(20, 20))
-
-    for index, image in enumerate(imgs):
-        ax = plt.subplot(1, len(imgs), index + 1)
-        plt.imshow(image)
-        plt.axis("off")
-    plt.show()
-
-    # w, h = imgs[0].size
-    # grid_w, grid_h = cols * w, rows * h
-    # grid = Image.new("RGB", size=(grid_w, grid_h))
-    #
-    # for i, img in enumerate(imgs):
-    #     x = i % cols * w
-    #     y = i // cols * h
-    #     grid.paste(img, box=(x, y))
-    #
-    # return grid
-
-
-image_grid(image.images, 1, 1)
+print(chat_completion.choices[0].message.content)
